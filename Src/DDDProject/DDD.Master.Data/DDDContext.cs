e5888c0580ee72bb50bbd81a29d6e3775552ae2f
Prins Carl Santoso <carl.santoso@outlook.com>
2017-11-09 14:13:21 +0800

    [C] Add Database Context with EF Core 2

---
+++ Src/DDDProject/DDD.Master.Data/DDDContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDD.Master.Data
+{
+    public class DDDContext : DbContext
+    {
+        public DDDContext(DbContextOptions<DDDContext> options) : base(options)
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            if (modelBuilder != null)
+            {
+
+            }
+        }
+    }
+}
